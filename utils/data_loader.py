ff2dcae827c4540a4566317c3adbe29ac3355cbd
Piotr Semberecki <piotr.semberecki@pwr.edu.pl>
2017-10-12 11:14:44 +0200

    added loader

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,6 +1,6 @@
 import os
 
-from utils.globals import DATA_SET_NAME, DATA_SET_PATH
+from utils.globals import DATA_SET_PATH
 
 
 def unpickle(file):
@@ -13,14 +13,18 @@ def unpickle(file):
 class DataSet(object):
     def __init__(self):
         self.meta = None
-        self.data_batch = []
+        self.data_batch = None
         self.test_batch = None
 
     def load_data(self, data_set_path):
         self.meta = unpickle(os.path.join(data_set_path, 'batches.meta'))
-        for x in range(1,6):
-            self.data_batch.append(unpickle(os.path.join(data_set_path, 'data_batch_' + str(x))))
+        self.data_batch = [self._get_path(data_set_path, x) for x in range(1, 6)]
         self.test_batch = unpickle(os.path.join(data_set_path, 'test_batch'))
+
+    @staticmethod
+    def _get_path(data_set_path, x):
+        path = os.path.join(data_set_path, 'data_batch_' + str(x))
+        return unpickle(path)
 
 
 if __name__ == "__main__":
