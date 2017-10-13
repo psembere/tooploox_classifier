57e8a965888b672a13e775181c657c66959ccb6e
Piotr Semberecki <piotr.semberecki@pwr.edu.pl>
2017-10-13 21:57:24 +0200

    added basic keras nets

--- utils/globals.py
+++ utils/globals.py
@@ -1,6 +1,8 @@
 import os
 
 UTILS_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+PROJECT_PATH = os.path.dirname(UTILS_DIR_PATH)
+
 DATA_SET_NAME = "cifar-10-batches-py"
 DATA_SET_PATH = os.path.join(UTILS_DIR_PATH, "..", DATA_SET_NAME)
 
