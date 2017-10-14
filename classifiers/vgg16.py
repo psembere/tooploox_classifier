55ebfcccb7b887356475ae446e141ea097928753
Piotr Semberecki <piotr.semberecki@pwr.edu.pl>
2017-10-14 21:57:15 +0200

    aded vgg16 features generation

--- classifiers/vgg16.py
+++ classifiers/vgg16.py
@@ -10,22 +10,18 @@ from utils.globals import PROJECT_PATH
 import os
 
 
-def get_vgg16_features(reload=False):
-    img_path = os.path.join(PROJECT_PATH, 'data', 'vgg16.npy')
-    a = os.path.isfile(img_path)
+def get_vgg16_features(model, img_path, pictures, reload=False, ):
     if os.path.isfile(img_path) and not reload:
         features = np.load(img_path)
     else:
-        features = generate_vgg16_features(img_path)
+        features = generate_vgg16_features(model, img_path, pictures)
     return features
 
 
-def generate_vgg16_features(img_path):
+def generate_vgg16_features(model, img_path, pictures):
     features = []
     idx = 0
-    model = VGG16(weights='imagenet', include_top=False)
-    data_set = get_data_set()
-    for img in data_set.training_pictures:
+    for img in pictures:
         x = image.img_to_array(img)
         x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
@@ -37,6 +33,17 @@ def generate_vgg16_features(img_path):
 
 
 if __name__ == "__main__":
-    features = get_vgg16_features()
+    model = VGG16(weights='imagenet', include_top=False)
 
-    print(features)
+    data_set = get_data_set()
+    pictures_train = data_set.training_pictures
+    pictures_test = data_set.testing_pictures
+
+    img_path_train = os.path.join(PROJECT_PATH, 'data', 'vgg16_train.npy')
+    img_path_test = os.path.join(PROJECT_PATH, 'data', 'vgg16_test.npy')
+
+
+    features_test = get_vgg16_features(model, img_path_test, pictures_test)
+    features_train = get_vgg16_features(model, img_path_train, pictures_train)
+
+    # print(features)
