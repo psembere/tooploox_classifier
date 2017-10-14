d780784996b1bc284427d3f8e32ddb4dfdfa7c9c
Piotr Semberecki <piotr.semberecki@pwr.edu.pl>
2017-10-14 13:21:16 +0200

    opencv imshow

--- stubs/hog_ocv_lib.py
+++ stubs/hog_ocv_lib.py
@@ -1,10 +1,12 @@
 import cv2
 import os
+
 hog = cv2.HOGDescriptor()
 
 LOCAL_PATH = os.path.dirname(os.path.realpath(__file__))
 im = cv2.imread(LOCAL_PATH + '/robot.jpg')
 h = hog.compute(im)
 
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.imshow('robot', im)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
