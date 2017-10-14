832bcc8a5206778ae9a63b1c129dfa7a8f65a95c
Piotr Semberecki <piotr.semberecki@pwr.edu.pl>
2017-10-14 20:15:48 +0200

    refactoring

--- classifiers/shallow_hog_svm.py
+++ classifiers/shallow_hog_svm.py
@@ -11,25 +11,32 @@ class FeatureLabelsDataSet(object):
         self.test_labels = None
 
 
-def linear_classifier(features):
-    prob = liblinearutil.problem(features.train_labels, features.train_features)
+def linear_classifier(feature_set):
+    prob = liblinearutil.problem(feature_set.train_labels, feature_set.train_features)
     param = liblinearutil.parameter('-c 4 -B 1')
     m = liblinearutil.train(prob, param)
-    p_label, p_acc, p_val = liblinearutil.predict(features.test_labels, features.test_features, m)
+    p_label, p_acc, p_val = liblinearutil.predict(feature_set.test_labels, feature_set.test_features, m)
 
 
-if __name__ == "__main__":
+def get_hog_features(overwrite=False, visualize=False):
     data_set = get_data_set()
     hog_train_data_set = HogTrainDataSet(data_set)
     hog_test_data_set = HogTestDataSet(data_set)
+    if visualize:
+        hog_train_data_set.display_picture_with_hog()
 
-    features = FeatureLabelsDataSet()
-    overwrite = True
-    train_features_list, features.train_labels = hog_train_data_set.generate_hog_features(overwrite)
-    test_features_list, features.test_labels = hog_test_data_set.generate_hog_features(overwrite)
+    feature_set = FeatureLabelsDataSet()
 
-    features.train_features = [batch.tolist() for batch in train_features_list]
-    features.test_features = [batch.tolist() for batch in test_features_list]
+    train_features_list, feature_set.train_labels = hog_train_data_set.generate_hog_features(overwrite)
+    test_features_list, feature_set.test_labels = hog_test_data_set.generate_hog_features(overwrite)
+    feature_set.train_features = [batch.tolist() for batch in train_features_list]
+    feature_set.test_features = [batch.tolist() for batch in test_features_list]
+
+    return feature_set
+
+
+if __name__ == "__main__":
+    features = get_hog_features()
 
     linear_classifier(features)
     print "kk"
