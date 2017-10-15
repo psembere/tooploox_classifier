008b10b509ddbd3d3a0b5f04a8190b46b6c17a8e
Piotr Semberecki <piotr.semberecki@pwr.edu.pl>
2017-10-15 18:22:31 +0200

    first tsne version

--- classifiers/vgg16.py
+++ classifiers/vgg16.py
@@ -53,7 +53,39 @@ class Vgg16FeatureDataSet(object):
         return self.model.predict(x)[0][0][0]
 
 
+def visualize_features(features):
+    import numpy as np
+    from matplotlib import pyplot as plt
+    from tsne import bh_sne
+
+    # load up data
+    x_data = features.train_features
+    y_data = features.train_labels
+
+    # convert image data to float64 matrix. float64 is need for bh_sne
+    x_data = np.asarray(x_data).astype('float64')
+    x_data = x_data.reshape((x_data.shape[0], -1))
+
+    # For speed of computation, only run on a subset
+    n = 10000
+    x_data = x_data[:n]
+    y_data = y_data[:n]
+
+    # perform t-SNE embedding
+    vis_data = bh_sne(x_data)
+
+    # plot the result
+    vis_x = vis_data[:, 0]
+    vis_y = vis_data[:, 1]
+
+    plt.scatter(vis_x, vis_y, c=y_data, cmap=plt.cm.get_cmap("jet", 10))
+    plt.colorbar(ticks=range(10))
+    plt.clim(-0.5, 9.5)
+    plt.show()
+
+
 if __name__ == "__main__":
-    features = Vgg16FeatureDataSet().get_features(overwrite=True)
-    linear_classifier(features)
+    features = Vgg16FeatureDataSet().get_features(overwrite=False)
+    # linear_classifier(features)
+    visualize_features(features)
     print("successfully ends")
--- download_dataset.py
+++ download_dataset.py
@@ -2,6 +2,8 @@ import os
 import tarfile
 import sys
 
+from classifiers.globals import SERIALIZED_DATA_PATH
+
 TAR_DATA_SET_FILENAME = "cifar-10-python.tar.gz"
 URL = "https://www.cs.toronto.edu/~kriz/" + TAR_DATA_SET_FILENAME
 
@@ -25,3 +27,6 @@ if __name__ == "__main__":
     download_data_set()
     extract()
     os.remove(TAR_DATA_SET_FILENAME)
+
+    if not os.path.exists(SERIALIZED_DATA_PATH):
+        os.makedirs(SERIALIZED_DATA_PATH)
