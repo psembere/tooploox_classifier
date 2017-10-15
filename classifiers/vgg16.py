b6513b91129077e119428b75f10aba2814f674b8
Piotr Semberecki <piotr.semberecki@pwr.edu.pl>
2017-10-15 15:41:50 +0200

    vgg16 refactor part 4

--- classifiers/vgg16.py
+++ classifiers/vgg16.py
@@ -10,56 +10,51 @@ from globals import SERIALIZED_DATA_PATH
 from svm_wrappers import FeaturesDataSet, linear_classifier
 
 
-def get_features(overwrite=False):
-    cnn_model = VGG16(weights='imagenet', include_top=False)
-    data_set = get_data_set()
-    feature_data_set = FeaturesDataSet()
+class Vgg16FeatureGenerator(object):
+    def __init__(self):
+        self.model = VGG16(weights='imagenet', include_top=False)
 
-    _set_test_features(data_set, feature_data_set, cnn_model, overwrite)
-    _set_train_features(data_set, feature_data_set, cnn_model, overwrite)
-    return feature_data_set
+    def get_features(self, overwrite=False):
+        data_set = get_data_set()
+        feature_data_set = FeaturesDataSet()
 
+        self._set_test_features(data_set, feature_data_set, overwrite)
+        self._set_train_features(data_set, feature_data_set, overwrite)
+        return feature_data_set
 
-def _set_train_features(data_set, feature_data_set, cnn_model, overwrite):
-    train_features_path = os.path.join(SERIALIZED_DATA_PATH, 'vgg16_train.npy')
-    train_features_np = _get_vgg16_features(cnn_model, train_features_path, data_set.training_pictures, overwrite)
+    def _set_train_features(self, data_set, feature_data_set, overwrite):
+        train_features_path = os.path.join(SERIALIZED_DATA_PATH, 'vgg16_train.npy')
+        train_features_np = self._get_vgg16_features(train_features_path, data_set.training_pictures, overwrite)
 
-    feature_data_set.train_features = train_features_np.tolist()
-    feature_data_set.train_labels = data_set.training_pictures_labels
+        feature_data_set.train_features = train_features_np.tolist()
+        feature_data_set.train_labels = data_set.training_pictures_labels
 
+    def _set_test_features(self, data_set, feature_data_set, overwrite):
+        test_features_path = os.path.join(SERIALIZED_DATA_PATH, 'vgg16_test.npy')
+        test_features_np = self._get_vgg16_features(test_features_path, data_set.testing_pictures, overwrite)
 
-def _set_test_features(data_set, feature_data_set, model, overwrite):
-    test_features_path = os.path.join(SERIALIZED_DATA_PATH, 'vgg16_test.npy')
-    test_features_np = _get_vgg16_features(model, test_features_path, data_set.testing_pictures, overwrite)
+        feature_data_set.test_features = test_features_np.tolist()
+        feature_data_set.test_labels = data_set.testing_pictures_labels
 
-    feature_data_set.test_features = test_features_np.tolist()
-    feature_data_set.test_labels = data_set.testing_pictures_labels
+    def _get_vgg16_features(self, features_path, pictures, overwrite):
+        if os.path.isfile(features_path) and not overwrite:
+            return np.load(features_path)
+        else:
+            vgg_features = np.array([self._get_cnn_code(idx, image) for idx, image in enumerate(pictures)])
+            np.save(features_path, vgg_features)
+            return vgg_features
 
-
-def _get_vgg16_features(cnn_model, img_path, pictures, overwrite=False):
-    if os.path.isfile(img_path) and not overwrite:
-        return np.load(img_path)
-    else:
-        return _generate_vgg16_features(cnn_model, img_path, pictures)
-
-
-def _generate_vgg16_features(model, features_path, pictures):
-    vgg_features = np.array([_get_cnn_code(model, idx, image) for idx, image in enumerate(pictures)])
-    np.save(features_path, vgg_features)
-    return vgg_features
-
-
-def _get_cnn_code(model, idx, image):
-    if idx % 1000 == 0:
-        print(idx)
-    img_converted = keras_image.img_to_array(image)
-    img_expanded = np.expand_dims(img_converted, axis=0)
-    x = preprocess_input(img_expanded)
-    return model.predict(x)[0][0][0]
+    def _get_cnn_code(self, idx, image):
+        if idx % 1000 == 0:
+            print(idx)
+        img_converted = keras_image.img_to_array(image)
+        img_expanded = np.expand_dims(img_converted, axis=0)
+        x = preprocess_input(img_expanded)
+        return self.model.predict(x)[0][0][0]
 
 
 if __name__ == "__main__":
-    features = get_features(overwrite=True)
+    features = Vgg16FeatureGenerator().get_features(overwrite=True)
 
     linear_classifier(features)
 
