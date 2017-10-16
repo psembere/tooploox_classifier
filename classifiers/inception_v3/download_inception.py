c1e5d94feba3ec29b230db7a4bf5dacf496e260b
Piotr Semberecki <piotr.semberecki@pwr.edu.pl>
2017-10-16 04:10:45 +0200

    reorganization

--- classifiers/inception_v3/download_inception.py
+++ classifiers/inception_v3/download_inception.py
@@ -1,20 +1,19 @@
-import distutils
 import os
 import shutil
 
 from classifiers.globals import EXTRACTED_DATA_SET_PATH, PROJECT_PATH, EXTRACTED_DATA_SET_DIR_NAME
-from classifiers.inception_v3.download_utils import download_data_set, extract_tar
+from download_utils import download_data_set, extract_tar
 
 INCEPTION_V3_FILENAME = "inception-2015-12-05.tgz"
 INCEPTION_V3_URL = "http://download.tensorflow.org/models/image/imagenet/" + INCEPTION_V3_FILENAME
 
 INCEPTION_PATH = os.path.join(PROJECT_PATH, "classifiers", "inception_v3")
-RESOURCES_PATH = os.path.join(INCEPTION_PATH, "resources")
+INCEPTION_RESOURCES_PATH = os.path.join(INCEPTION_PATH, "resources")
 
 DOWNLOADED_INCEPTION = os.path.join(INCEPTION_PATH, INCEPTION_V3_FILENAME)
-MOVED_INCEPTION = os.path.join(RESOURCES_PATH, INCEPTION_V3_FILENAME)
+MOVED_INCEPTION = os.path.join(INCEPTION_RESOURCES_PATH, INCEPTION_V3_FILENAME)
 
-DATA_SETS_PATH = os.path.join(RESOURCES_PATH, "datasets")
+DATA_SETS_PATH = os.path.join(INCEPTION_RESOURCES_PATH, "datasets")
 
 CIFAR_COPY = os.path.join(DATA_SETS_PATH, EXTRACTED_DATA_SET_DIR_NAME)
 
@@ -24,10 +23,10 @@ def prepare_inception():
     download_data_set(INCEPTION_V3_URL, DOWNLOADED_INCEPTION)
     print ("inception data downloaded")
 
-    os.mkdir(RESOURCES_PATH)
+    os.mkdir(INCEPTION_RESOURCES_PATH)
 
     shutil.move(DOWNLOADED_INCEPTION, MOVED_INCEPTION)
-    extract_tar(MOVED_INCEPTION, RESOURCES_PATH)
+    extract_tar(MOVED_INCEPTION, INCEPTION_RESOURCES_PATH)
     os.remove(MOVED_INCEPTION)
 
     os.mkdir(DATA_SETS_PATH)
--- download_dataset.py
+++ download_dataset.py
@@ -1,22 +1,23 @@
 import os
 
 from classifiers.globals import SERIALIZED_DATA_PATH, EXTRACTED_DATA_SET_DIR_NAME
-from classifiers.inception_v3.download_inception import prepare_inception
-from classifiers.inception_v3.download_utils import download_data_set, extract_tar
+from classifiers.inception_v3.download_inception import prepare_inception, INCEPTION_RESOURCES_PATH
+from download_utils import download_data_set, extract_tar
 
 TAR_DATA_SET_FILENAME = "cifar-10-python.tar.gz"
 URL = "https://www.cs.toronto.edu/~kriz/" + TAR_DATA_SET_FILENAME
 
 if __name__ == "__main__":
     if not os.path.exists(EXTRACTED_DATA_SET_DIR_NAME):
-        print ("data set downloading")
+        print ("cifar data set downloading")
         download_data_set(URL, TAR_DATA_SET_FILENAME)
         extract_tar(TAR_DATA_SET_FILENAME)
         os.remove(TAR_DATA_SET_FILENAME)
     else:
-        print("data set loaded")
+        print("cifar data set loaded")
 
     if not os.path.exists(SERIALIZED_DATA_PATH):
         os.makedirs(SERIALIZED_DATA_PATH)
 
-    prepare_inception()
+    if not os.path.exists(INCEPTION_RESOURCES_PATH):
+        prepare_inception()
