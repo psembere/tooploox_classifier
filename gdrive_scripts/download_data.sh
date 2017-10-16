d3abf35471c13e33a7e8e7e1d7a5100a315cdf3b
Piotr Semberecki <piotr.semberecki@pwr.edu.pl>
2017-10-16 21:40:00 +0200

    added final results

--- gdrive_scripts/download_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-./gdown.pl "https://drive.google.com/uc?export=download&id=0Bz7KyqmuGsilT0J5dmRCM0ROVHc" vgg16_weights.h5
-./gdown.pl "https://drive.google.com/uc?export=download&id=0Bz7KyqmuGsilZ2RVeVhKY0FyRmc" vgg19_weights.h5
-
